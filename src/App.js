f34bed7420c582927e9746da809542125427cae7
RehamanRamz <rehaman.ramzaula@gmail.com>
2023-04-01 23:30:28 +0300

    added animation to robot

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import { useState } from "react";
 import { animate, motion } from "framer-motion";
 import Header from "./shared/Header";
-import robotesimg from "./image/robotes.svg";
-import robotesimg1 from "./image/robotes1.svg";
-import dotsimg from "./image/dots.svg";
-import bgimg from "./image/BG.svg";
+import robot from "./image/robot.svg";
+
+import phone from "./image/phone.svg";
+import dots from "./image/dots.svg";
+import bgimg from "./image/background.svg";
 import { BsArrowRight } from "react-icons/bs";
+import Typical from "react-typical";
 function App() {
   const imgVariant = {
-    hidden: {
-      y: 200,
-      opacity: 0,
-    },
     animate: {
-      y: 100,
-      opacity: 1,
-    },
-    exit: {
-      y: 200,
-      opacity: 0,
+      y: [50, 0, 50],
     },
   };
   const dotsVariant = {
@@ -32,37 +25,7 @@ function App() {
       opacity: 0,
     },
   };
-  const text = "Artifical Intelligence";
-  const letters = Array.from(text);
-  const container = {
-    hidden: { opacity: 0 },
-    visible: (i = 1) => ({
-      opacity: 1,
-      transition: { staggerChildren: 0.03, delayChildren: 0.04 * i },
-    }),
-  };
-  const child = {
-    visible: {
-      opacity: 1,
-      x: 0,
-      y: 0,
-      transition: {
-        type: "spring",
-        damping: 12,
-        stiffness: 100,
-      },
-    },
-    hidden: {
-      opacity: 0,
-      x: -20,
-      y: 10,
-      transition: {
-        type: "spring",
-        damping: 12,
-        stiffness: 100,
-      },
-    },
-  };
+
   return (
     <div
       className=" min-h-screen text-white bg-cover bg-center bg-no-repeat"
@@ -70,30 +33,32 @@ function App() {
     >
       <Header />
       <section className="absolute min-h-screen inset-0 flex justify-center items-center">
-        <motion.div className="container mx-auto w-[400px]">
+        <motion.div className="container mx-auto ">
           <div className="flex justify-center items-center flex-col  mb-5 text-center">
+            <div className=" absolute  w-64">
+              <img src={phone} />
+            </div>
             <motion.img
               variants={imgVariant}
               transition={{
                 repeat: Infinity,
-                repeatDelay: 1,
-                duration: 2,
+                duration: 5,
               }}
               animate="animate"
               initial="hidden"
-              src={robotesimg}
+              src={robot}
               alt="robot"
-              className="w-96 h-96"
+              className="w-4/5 h-96"
             />
             <div>
               <motion.img
-                src={dotsimg}
+                src={dots}
                 style={{}}
                 className="cursor-pointer absolute inset-0 w-full"
                 variants={dotsVariant}
                 initial={{ opacity: 0, scale: 0.9 }}
                 animate={{ opacity: 1, scale: 0.9 }}
-                transition={{ repeat: Infinity, repeatDelay: 1, duration: 3 }}
+                transition={{ repeat: Infinity, duration: 5 }}
                 // transition={{
                 //   repeat: Infinity,
                 //   repeatDelay: 4,
@@ -102,7 +67,8 @@ function App() {
                 // }}
               />
             </div>
-            <motion.div
+
+            {/* <motion.div
               style={{ overflow: "hidden", display: "flex", fontSize: "2rem" }}
               variants={container}
               initial="hidden"
@@ -117,7 +83,14 @@ function App() {
                   {letter === " " ? "\u00A0" : letter}
                 </motion.h1>
               ))}
-            </motion.div>
+            </motion.div> */}
+
+            <Typical
+              steps={["Artifical", 1000, "Artifical Intelligence", 1000]}
+              loop={Infinity}
+              wrapper="h1"
+              className="text-[36px] md:text-5xl lg:text-[60px] my-10 font-extrabold leading-2 relative z-10"
+            />
             {/* <motion.h1
               className=" text-[36px] md:text-5xl lg:text-[60px] font-extrabold mb-5 relative z-10 leading-tight"
               animate={{ opacity: 1, scale: 1 }}
@@ -130,24 +103,24 @@ function App() {
             >
               Artifical Intelligence
             </motion.h1> */}
-            <motion.p
-              animate={{ opacity: 1, scale: 1 }}
-              transition={{
-                duration: 1,
-                delay: 0.1,
-                ease: [0.5, 0.71, 1, 1.5],
-              }}
-              initial={{ opacity: 0, scale: 0.5 }}
+            <p
+            // animate={{ opacity: 1, scale: 1 }}
+            // transition={{
+            //   duration: 1,
+            //   delay: 0.1,
+            //   ease: [0.5, 0.71, 1, 1.5],
+            // }}
+            // initial={{ opacity: 0, scale: 0.5 }}
             >
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor
               rem perspiciatis doloribus corporis,
-            </motion.p>
+            </p>
             <div className="flex justify-center space-x-3   mt-5 ">
               <button className="flex  items-center text-md bg-gradient-to-r from-[#2a096b] from-10% via-[#2a096b] via-30% to-[#7a3f9c] to-90% hover:bg-blue-700 text-white font-bold py-2 px-4   rounded-full">
                 Read More <BsArrowRight className="ml-2" />
               </button>
-              <button className="text-md text-white bg-transparent  font-semibold hover:text-white py-2 px-4 border border-gradient-to-r from-indigo-500 via-purple-500 rounded-full w-32">
-                Tell a friend
+              <button className="flex  items-center text-md text-white bg-transparent  font-semibold hover:text-white py-2 px-4 border border-color-gradient-to-r from-[#2a096b]  via-[#2a096b] via-30% to-[#7a3f9c] to-90% rounded-full ">
+                Tell a friend <BsArrowRight className="ml-2" />
               </button>
             </div>
           </div>
